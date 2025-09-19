f10564d0454688ebefb80684c165b6c995eab7c6
Rahul Vijay Wagh <147045551+rahul0077556@users.noreply.github.com>
2025-09-19 20:23:15 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,13 +1,7 @@
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', include('admin.login.urls')),
-    path('dashboard/', include('admin.dashboard.urls')),
-    path('genre/', include('admin.genre.urls')),
-    path('moods/', include('admin.mood.urls')),
-    path('artist/', include('admin.artist.urls')),
-    path('songs/', include('admin.song.urls')),
-    path('users/', include('admin.user.urls')),
-    path('favorites/', include('admin.favorite.urls')),
-    path('homepage/', include('admin.homepage.urls')),
+    path('index/', views.index, name='favorite-index'),
+    path('details/<int:id>/', views.details, name='favorite-details'),
 ]
